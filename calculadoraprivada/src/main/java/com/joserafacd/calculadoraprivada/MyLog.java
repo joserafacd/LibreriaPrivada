ed37a41cd940f03d158bacf52d7366395f3bcc67
joserafacd <joserafacd@hotmail.com>
2021-10-20 10:26:37 -0600

    Inicio del Proyecto

---
+++ calculadoraprivada/src/main/java/com/joserafacd/calculadoraprivada/MyLog.java
@@ -0,0 +1,9 @@
+package com.joserafacd.calculadoraprivada;
+
+import android.util.Log;
+
+public class MyLog {
+    public static void d(String TAG, String message){
+        Log.d(TAG, message);
+    }
+}
