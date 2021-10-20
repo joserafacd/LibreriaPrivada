f60fa4a523d98a4f4b2713741c6374baf9942fcf
joserafacd <joserafacd@hotmail.com>
2021-10-20 12:19:02 -0600

    metodo i

--- calculadoraprivada/src/main/java/com/joserafacd/calculadoraprivada/MyLog.java
+++ calculadoraprivada/src/main/java/com/joserafacd/calculadoraprivada/MyLog.java
@@ -10,4 +10,8 @@ public class MyLog {
     public static void v(String message){
         Log.v("Prueba",message);
     }
+
+    public static void i(String message){
+        Log.i("Hola", message);
+    }
 }
