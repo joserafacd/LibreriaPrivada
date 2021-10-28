73b194759d2abf0b759805501298ea44529d1165
Jose Carcamo <jose.carcamo@digitalreef.com>
2021-10-28 14:19:44 -0600

    inlusion de libreria dentro de la libreria

--- app/src/main/java/com/joserafacd/libreriaprivada/MainActivity.java
+++ app/src/main/java/com/joserafacd/libreriaprivada/MainActivity.java
@@ -3,6 +3,7 @@ package com.joserafacd.libreriaprivada;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.joserafacd.calculadoraprivada.MyLog;
 
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MyLog myLog = new MyLog();
+        Log.d("versionName",myLog.versionName(this));
     }
 }
